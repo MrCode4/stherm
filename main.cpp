5bb2beb5d4686ee048060f732e2eddd244045369
m.alizadeh <m.alizadeh91@gmail.com>
2023-11-02 16:04:28 +0330

    feat: Add setTimezone, getCPUInfo, setBrightness and getStartMode.

--- main.cpp
+++ main.cpp
@@ -6,10 +6,157 @@
 #include <QFontDatabase>
 #include <QSysInfo>
 #include <QProcess>
+#include <QFile>
+#include <QRegularExpression>
+#include <QDebug>
 
+//! todo: Move to daemon
+void exportGPIOPin(int pinNumber) {
+    QFile exportFile("/sys/class/gpio/export");
+    if (!exportFile.open(QIODevice::WriteOnly | QIODevice::Text)) {
+        qDebug() << Q_FUNC_INFO << __LINE__ << "Failed to open export file.";
+        return;
+    }
+
+    // Convert pinNumber to string
+    QString pinString = QString::number(pinNumber);
+
+    // Write the pin number to the export file
+    QTextStream out(&exportFile);
+    out << pinString;
+    exportFile.close();
+
+
+    QString directionFilePath = QString("/sys/class/gpio/gpio%0/direction").arg(pinNumber);
+    QFile directionFile(directionFilePath);
+
+    if (!directionFile.open(QIODevice::WriteOnly | QIODevice::Text)) {
+        qDebug() << Q_FUNC_INFO << __LINE__ << "Failed to open direction file for pin " << pinNumber;
+        return;
+    }
+
+    QTextStream outIn(&directionFile);
+    outIn << "in";
+
+    directionFile.close();
+
+}
+
+//! todo: Move to daemon
+int getStartMode (int pinNumber) {
+    exportGPIOPin(pinNumber);
+
+    // Define the file path
+    QString filePath = QString("/sys/class/gpio/gpio%0/value").arg(QString::number(pinNumber));
+
+    QFile file(filePath);
+    if (!file.open(QIODevice::ReadOnly | QIODevice::Text)) {
+        qDebug() << Q_FUNC_INFO << __LINE__ << "Failed to open the file.";
+        return -1;
+    }
+
+    QTextStream in(&file);
+    QString value;
+    in >> value; // Read the content of the file
+
+    qDebug() << Q_FUNC_INFO << __LINE__ << value;
+
+    file.close();
+
+    int result = (value.trimmed() == "0") ? 1 : 0;
+    return result;
+
+}
+
+//! Get CPU info
+//! todo: Move to daemon
+void getCPUInfo() {
+    QFile file("/proc/cpuinfo");
+    if (!file.open(QIODevice::ReadOnly | QIODevice::Text)) {
+        qDebug() << Q_FUNC_INFO << __LINE__ <<"Failed to open the file.";
+        return;
+    }
+
+    QString cpuInfo;
+    QString serialNumber;
+    QString line = file.readLine();
+
+    while (!line.isEmpty()) {
+        cpuInfo.append(line).append('\n');
+
+        if (line.startsWith("Serial")) {
+            QRegularExpression re("Serial:\\s*([A-Fa-f0-9]+)");
+            QRegularExpressionMatch match = re.match(line);
+            if (match.hasMatch()) {
+                QString serialNumberHex = match.captured(1);
+                serialNumberHex = serialNumberHex.simplified();  // Remove extra spaces
+                QByteArray snByteArray = QByteArray(serialNumberHex.toLatin1());
+                serialNumber = QByteArray::fromHex(snByteArray);
+            }
+        }
+
+        line = file.readLine();
+    }
+
+    file.close();
+
+    qDebug() << Q_FUNC_INFO << __LINE__ << "cpuInfo: " << cpuInfo;
+    qDebug() << Q_FUNC_INFO << __LINE__ << "Serial Number: " << serialNumber;
+}
+
+//! setBrightness
+//! todo: Move to daemon
+void setBrightness(int value) {
+    QFile brightnessFile("/sys/class/backlight/backlight_display/brightness");
+    if (!brightnessFile.open(QIODevice::WriteOnly | QIODevice::Text)) {
+        qDebug() << Q_FUNC_INFO << __LINE__  << "Failed to open brightness file.";
+        return;
+    }
+
+    QTextStream out(&brightnessFile);
+    out << value; // Write the desired brightness value
+    brightnessFile.close();
+
+    qDebug() << "Brightness set successfully!";
+}
+
+//! Set time zone
+//! todo: Move to daemon
+void setTimeZone(int offset) {
+    QString timezoneName = "GMT";
+    QString offsetStr = (offset >= 0) ? "+" : "";
+    offsetStr += QString::number(offset);
+
+    QString timezonePath = "/usr/share/zoneinfo/Etc/";
+    QString linkPath = "/etc/localtime";
+
+    QString timezoneFile = timezonePath + timezoneName + offsetStr;
+
+    if (!QFile::exists(timezoneFile)) {
+        qDebug() << Q_FUNC_INFO << __LINE__  << "Timezone file not found.";
+        return;
+    }
+
+    int exitCode = QProcess::execute("ln", {"-sf", timezoneFile, linkPath});
+
+    if (exitCode >= 0) {
+        qDebug() << Q_FUNC_INFO << __LINE__  << "Timezone set to" << timezoneFile;
+    }
+}
 
 int main(int argc, char *argv[])
 {
+    qDebug() << Q_FUNC_INFO << __LINE__ << "getStartMode: " << getStartMode(90);
+
+    // CPU info example
+    getCPUInfo();
+
+    // Brightness example
+    setBrightness(50);
+
+    // Time zoon example
+    setTimeZone(8);
+
     //! Enable virtual keyboard
     qputenv("QT_IM_MODULE", QByteArray("qtvirtualkeyboard"));
 
