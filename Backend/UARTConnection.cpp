df1738f5b817cfa9794158f108b129dc15ac152c
m.alizadeh <m.alizadeh91@gmail.com>
2023-11-05 21:58:33 +0330

    feat: Add some methods in UARTConnection class.

--- Backend/UARTConnection.cpp
+++ Backend/UARTConnection.cpp
@@ -4,3 +4,67 @@ UARTConnection::UARTConnection()
 {
 
 }
+
+void UARTConnection::initConnection()
+{
+    if (this->mSerial->isOpen()) {
+        this->mSerial->close();
+
+        return;
+    }
+
+    QObject::connect(mSerial, &QSerialPort::readyRead, this, &UARTConnection::onReadyRead);
+
+    // Handle errors
+    QObject::connect(mSerial, &QSerialPort::errorOccurred, [this](QSerialPort::SerialPortError error) {
+
+        QString errorMessage = mSerial->errorString();
+        qDebug() << Q_FUNC_INFO << __LINE__ << error << errorMessage;
+        emit connectionError(errorMessage);
+    });
+}
+
+bool UARTConnection::connect()
+{
+    // Open Serial port & send beacon, ping packets to bring device into connected state
+    if (!this->mSerial->isOpen()) {
+        this->mSerial->open(QIODevice::ReadWrite);
+    }
+
+    // If open fails then return with an error
+    if (!this->mSerial->isOpen()) {
+        qDebug() << (QString("Can't open %1,%2 error code %3")
+                         .arg(this->mSerial->portName())
+                         .arg(this->mSerial->baudRate())
+                         .arg(this->mSerial->error()));
+        return false;
+    }
+
+    return this->mSerial->isOpen();
+}
+
+
+bool UARTConnection::disconnect()
+{
+    // Close Serial port if open
+    if (this->mSerial->isOpen())
+    {
+        this->mSerial->close();
+    }
+
+    return !this->mSerial->isOpen();
+}
+
+bool UARTConnection::isConnected()
+{
+    return this->mSerial->isOpen();
+}
+
+bool UARTConnection::writeData(QByteArray data) {
+    mSerial->write(data);
+}
+
+void UARTConnection::onReadyRead()
+{
+
+}
--- Backend/UARTConnection.h
+++ Backend/UARTConnection.h
@@ -1,5 +1,8 @@
 #pragma once
 
+#include <QVariant>
+#include <QSerialPort>
+#include <QThread>
 
 /*! ***********************************************************************************************
  * Propagte a connection with UART port and read data
@@ -11,10 +14,49 @@ public:
     /* public Constractor
      * ****************************************************************************************/
 
-    UARTConnection();
+    explicit UARTConnection();
 
     /* public functions
      * ****************************************************************************************/
 
+    QByteArray sendCommand(QByteArray command);
+
+    QVariant connect(const QString &portName, qint32 baudRate);
+
+    //! Configure and initalize UART connection
+    void initConnection();
+
+
+    //! Connect to device via serial port
+    //! return true connection success
+    //! return false connection failure
+    bool connect();
+
+    //! Disconnect from device
+    //! return false device still connected
+    //! return true device disconnected
+    bool disconnect();
+
+
+    //! Check if the serial port is still connected
+    //! return true connected
+    //! return false disconnected
+    bool isConnected();
+
+    //! Write Data
+    bool writeData(QByteArray data);
+
+signals:
+    void connectionError(QString error);
+
+private slots:
+
+    //! Call when new data is available for reading from the device's current read channel.
+    void onReadyRead();
+
+
+private:
+
+    QSerialPort *mSerial;
 };
 
