ce84f60a0a5332b6e9e5693a7e01115b8155661a
Tony Ward <tony@lucidtron.com>
2023-11-13 08:04:55 +1100

    file rename

--- Backend/Core/Sync.h
+++ Backend/Core/Sync.h
@@ -3,7 +3,7 @@
 #include <QObject>
 #include <QtNetwork>
 
-#include "NetWorkWorker.h"
+#include "NetworkWorker.h"
 
 /*! ***********************************************************************************************
  * This class manage sync requests.
