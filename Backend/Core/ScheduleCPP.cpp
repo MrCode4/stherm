9354e932e6edebac5d05dbd736cabd9bb3ee724e
m.alizadeh <m.alizadeh91@gmail.com>
2023-12-31 14:14:09 +0330

    Add schedule defaults.

--- Backend/Core/ScheduleCPP.cpp
+++ Backend/Core/ScheduleCPP.cpp
@@ -3,6 +3,11 @@
 ScheduleCPP::ScheduleCPP(QSObjectCpp *parent) :
     QSObjectCpp(parent)
 {
+    // Defaults
+    enable     = true;
+    temprature = 0; // Celsius
+    humidity   = 0; // Percentage
+
     connect(this, &ScheduleCPP::enableChanged, this, [=]() {
         if (!enable) {
             _active = false;
