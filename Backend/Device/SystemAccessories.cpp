93a3c889f2bde14c7f3fdaa2c3e30219534b2025
Benyamin Saedi <benyamin.saedi@gmail.com>
2024-01-05 22:27:16 +0330

    In the Accessories the default should be "None"

--- Backend/Device/SystemAccessories.cpp
+++ Backend/Device/SystemAccessories.cpp
@@ -3,7 +3,7 @@
 SystemAccessories::SystemAccessories(QSObjectCpp *parent) :
     QSObjectCpp{parent}
 {
-    setSystemAccessories(AppSpecCPP::Humidifier, AppSpecCPP::T1PWRD);
+    setSystemAccessories(AppSpecCPP::Humidifier, AppSpecCPP::None);
 }
 
 void SystemAccessories::setSystemAccessories(AppSpecCPP::AccessoriesType accessoriesType, AppSpecCPP::AccessoriesWireType wireType) {
