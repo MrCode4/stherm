df9c43838838c3ab1b00e528dd3acdcfd5fb7184
B Saedi <benyamin.saedi@gmail.com>
2024-03-20 13:02:45 +0300

    change deltaCorrection

--- Backend/DeviceControllerCPP.h
+++ Backend/DeviceControllerCPP.h
@@ -189,8 +189,8 @@ private:
     //! Temperature correction parameters
     double mDeltaTemperatureIntegrator;
     const double TEMPERATURE_INTEGRATOR_DECAY_CONSTANT = 0.99721916;
-    const double TEMPERATURE_COMPENSATION_OFFSET = 0.25;
-    const double TEMPERATURE_COMPENSATION_SCALER = 0.8*3.1/360;
+    const double TEMPERATURE_COMPENSATION_OFFSET = 0.25 + 2.0 / 1.8;
+    const double TEMPERATURE_COMPENSATION_SCALER = 0.8 * 3.1 / 360;
     double deltaCorrection()
     {
         return  TEMPERATURE_COMPENSATION_OFFSET + mDeltaTemperatureIntegrator * TEMPERATURE_COMPENSATION_SCALER;
