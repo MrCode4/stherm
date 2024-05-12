0d6e87191a0f2e4ef0e4202bf36cfe92d654cce9
Mostafa <m.alizadeh91@gmail.com>
2024-05-12 23:24:37 +1000

    fix: prevent need-auth signal from being emitted for out-of-range networks

--- Backend/Network/Nmcli/NmcliInterface.cpp
+++ Backend/Network/Nmcli/NmcliInterface.cpp
@@ -498,6 +498,8 @@ void NmcliInterface::setupObserver()
     });
 
     connect(mNmcliObserver, &NmcliObserver::wifiNeedAuthentication, this, [&](const QString& ssid) {
+        if (ssid.isEmpty()) return;
+
         for (WifiInfo* wifi : mWifis) {
             if (wifi->ssid() == ssid || wifi->incorrectSsid() == ssid) {
                 emit wifiNeedAuthentication(wifi);
